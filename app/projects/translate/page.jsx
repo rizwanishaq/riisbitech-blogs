61e7b96877a384fa979294dff273f38d640c228f
rizwan ishaq <rizwanishaq@gmail.com>
2023-09-08 12:35:49 +0200

    animation

--- app/projects/translate/page.jsx
+++ app/projects/translate/page.jsx
@@ -31,7 +31,11 @@ const Translate = () => {
   return (
     <section className="flex flex-col items-center space-y-12 max-w-2xl w-full mx-auto">
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-        <div className="absolute inset-0 bg-gradient-to-r from-brightRed to-light-brightRedLight shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div
+          className={`absolute inset-0 bg-gradient-to-r from-brightRed to-light-brightRedLight shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl ${
+            processing ? "animate-spin" : ""
+          }`}
+        ></div>
         <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
           <div className="max-w-md mx-auto">
             <div>
